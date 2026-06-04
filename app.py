83fa108a1e34b95558e1259405b8427558bd3402
Vinya H G <vinyagowda90@gmail.com>
2026-06-04 12:23:22 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Fitbit Analytics",
+    page_icon="🏃",
+    layout="wide"
+)
+
+st.title(
+    "🏃 Fitbit Fitness Analytics Dashboard"
+)
+
+st.markdown(
+"""
+Analyze fitness,
+sleep,
+heart-rate and activity trends.
+"""
+)
+
+st.success(
+"""
+Navigate using the sidebar.
+"""
+)
